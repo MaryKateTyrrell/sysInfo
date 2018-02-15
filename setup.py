62ac9495064bdd767f453c5ce723aee289884a50
Mary Kate Tyrrell <mary.tyrrell@ucdconnect.ie>
2018-02-15 10:32:36 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+'''
+Created on 25 Jan 2018
+
+@author: mary-katetyrrell
+'''
+
+from setuptools import setup
+
+
+setup(name="systemInfo", 
+      version="0.1",
+      description="Basic System Info for COMP30670",
+      url="",
+      author="Mary-Kate Tyrrell",
+      author_email="mary.tyrrell@ucdconnect.ie",
+      license="GPL3",
+      packages=['sysInfo'],
+      entry_points={'console_scripts':['comp30670_systeminfo=sysInfo.main:main'],
+                    }
+      )
---
+++ sysInfo/main.py
@@ -0,0 +1,13 @@
+'''
+Created on 25 Jan 2018
+
+@author: Mary Catherine Tyrrell
+'''
+import platform
+
+def main():
+    print(platform.platform())
+    return
+
+if __name__ == '__main__':
+    main()
